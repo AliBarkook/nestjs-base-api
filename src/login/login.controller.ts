5c9496571232ea3f73f2074e934af2de3875adc7
a.barkook <alibarkook@gmail.com>
2021-10-26 08:51:33 +0330

    feat -> implement login controller and service

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { LoginDTO } from 'src/shared/login.DTO';
+import { LoginService } from './login.service';
 
 @Controller('login')
-export class LoginController {}
+export class LoginController {
+    constructor(private readonly loginService: LoginService) {}
+
+
+    @Post()
+    create(@Body() loginDTO: LoginDTO) {
+        return this.loginService.loginUser(loginDTO);
+    }
+}
